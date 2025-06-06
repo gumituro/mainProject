c8f0c29b77f84b2731f58b641c311828c676475e
ShimaEmami <shima2006emami@gmail.com>
2025-06-06 12:07:53 +0330

    chunk

---
+++ Assets/scripts/DamagePlayer.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            //FindFirstObjectByType<HealthControler>().Damage();
+            // HealthControler.instance.Damage(); 
+        }
+
+    }
+}
