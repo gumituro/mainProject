8c5e75f0e60b22685ea2de2ca4a864844993ce96
ShimaEmami <shima2006emami@gmail.com>
2025-06-09 17:58:24 +0330

    just stair case and cave

---
+++ Assets/scripts/ChunkLoaderWithTeleport.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ChunkLoaderWithTeleport : MonoBehaviour
+{
+    public GameObject chunkToActivate;
+    public Transform teleportTarget;
+
+    private bool hasActivated = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (hasActivated) return;
+
+        if (other.CompareTag("Player"))
+        {
+            if (chunkToActivate != null)
+                chunkToActivate.SetActive(true);
+            
+            if (teleportTarget != null)
+                other.transform.position = teleportTarget.position;
+
+            hasActivated = true;
+        }
+    }
+}
